9a97e49392c96062b8a45ba235b16a10af8d8b0a
Yuhuan He <168407654+yhh1008@users.noreply.github.com>
2026-02-11 17:10:37 -0800

    Update Vite base for GitHub Pages

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/diffusion-avatar/', 
+  base: '/Diffusion_Avatar_Generator/',
   plugins: [svelte()],
   build: {
     outDir: '../docs'
